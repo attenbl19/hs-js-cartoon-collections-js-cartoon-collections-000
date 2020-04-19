32318a72b93416b97a47c290c5dd1009464ac3a7
Lida Attenborough <attenboroughl@protonmail.com>
2020-04-19 21:04:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
+//Dwarf Roll Call
+
 function dwarfRollCall(dwarves) {
-}
+  
+  const numberedDwarf = [] //this array will store the new elements number and name
+   
+  for (let i=0; i<dwarves.length; i++) {
+    
+    numberedDwarf.push(`${[i+1]}. ${dwarves[i]}`)
+  }
+  
+   return numberedDwarf.join(" ") + " "; //should return string of number and name
+  
+} 
+
 
 function summonCaptainPlanet(planeteerCalls){
+  
+  const newPlaneteerCalls = [];
+  
+  for (let i=0; i<planeteerCalls.length; i++) {
+    
+    newPlaneteerCalls.push(`${planeteerCalls[i].toUpperCase()}!`);
+}
+  
+  return newPlaneteerCalls; 
+
 }
 
 function longPlaneteerCalls(words) {
+  
+  for (let i=0; i<words.length; i++) {
+    
+    if (words[i].length > 4) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+  
 }
 
 function findTheCheese (foods) {
+  
+  const cheeseArray = ["cheddar", "gouda", "camembert"]
+  
+  for(let i=0; i<foods.length; i++) {
+    
+    for(let j=0; i<cheeseArray.length; j++) {
+      
+      if (foods[i] === cheeseArray[j]) {
+        
+        return foods[i];
+      }
+    }
+}
+return "no cheese!";
 }
